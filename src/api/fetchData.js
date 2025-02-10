05d0110d7cbf4b967bbe5cee5533bd1973c51f06
new name <new name>
2025-02-10 12:14:50 +0600

    first commit

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchData = async function (query, page) {
-  const API_KEY = import.meta.env.VITE_API_KEY;
+  const API_KEY = "48652478-8ab3f1e411d45e2d55c4313cc";
   const API = `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=16&lang=ru=en`;
 
   const { data } = await axios.get(API);
